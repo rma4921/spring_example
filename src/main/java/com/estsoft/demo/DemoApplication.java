cfcf60c08a334d2d38bf40636fd35b548e9c1c4c
rma4921 <rma4921@naver.com>
2025-04-02 16:49:38 +0900

    Annotation Process

--- src/main/java/com/estsoft/demo/DemoApplication.java
+++ src/main/java/com/estsoft/demo/DemoApplication.java
@@ -11,13 +11,14 @@ import java.util.Arrays;
 public class DemoApplication {
 
 	public static void main(String[] args) {
+		/*
 		// 스프링 컨테이너가 관리하는 빈 조회
 		ApplicationContext context = new AnnotationConfigApplicationContext(DemoApplication.class);
 		String[] beanList = context.getBeanDefinitionNames();
 
 		System.out.println("===== 스프링 컨테이너가 관리하는 빈 목록 =====");
 		Arrays.stream(beanList).forEach(System.out::println);
-
+		*/
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
