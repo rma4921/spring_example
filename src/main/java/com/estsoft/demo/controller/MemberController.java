cfc78ef1b5f78cb210fe3aeb063d4d2c813a62ef
rma4921 <rma4921@naver.com>
2025-04-03 17:44:45 +0900

    JPA 2일차 특정 ID select, delete 메서드 추가

--- src/main/java/com/estsoft/demo/controller/MemberController.java
+++ src/main/java/com/estsoft/demo/controller/MemberController.java
@@ -4,8 +4,7 @@ import com.estsoft.demo.repository.Member;
 import com.estsoft.demo.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -18,6 +17,27 @@ public class MemberController {
     @GetMapping("/members")
     public List<Member> showMembers() {
         return memberService.getMemberAll();
+    }
+
+    @ResponseBody
+    @PostMapping("/members")
+    public Member saveMember(@RequestBody Member member) {
+        return memberService.insertMember(member);
+    }
+
+    // GET /members/{id} 특정 id 해당하는 값 가져오기 // findById 이용
+    @ResponseBody
+    @GetMapping("/members/{id}")
+    public Member selectMemberById(@PathVariable Long id) {
+        return memberService.selectMemberById(id);
+    }
+
+    // DELETE /students/{id} deleteById 이용
+    @ResponseBody
+    @DeleteMapping("/members/{id}")
+    public String deleteStudent(@PathVariable Long id) {
+        memberService.deleteMemberById(id);
+        return "삭제 성공";
     }
 
     @GetMapping("/hi")
--- src/main/java/com/estsoft/demo/service/MemberService.java
+++ src/main/java/com/estsoft/demo/service/MemberService.java
@@ -5,6 +5,7 @@ import com.estsoft.demo.repository.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor // final이나 @NonNull이 붙은 필드만 포함하는 생성자를 자동으로 생성.
@@ -20,4 +21,22 @@ public class MemberService {
     public List<Member> getMemberAll() {
         return memberRepository.findAll();
     }
+
+    // member 테이블에 insert 쿼리
+    public Member insertMember(Member member) {
+        Member savedMember = memberRepository.save(member);
+        return savedMember;
+    }
+
+    // ID로 특정 멤버 조회
+    public Member selectMemberById(Long id) {
+        Optional<Member> optMember = memberRepository.findById(id);
+        return optMember.orElseGet(Member::new);
+        // return optMember.orElse(new Member()); // 안티패턴
+    }
+
+    public void deleteMemberById(Long id) {
+        memberRepository.deleteById(id);
+    }
+
 }
